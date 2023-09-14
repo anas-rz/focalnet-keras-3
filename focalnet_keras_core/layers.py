00296e49b5d6be1f7981501594c9dda43dde3050
Muhammad Anas Raza <memanasraza@gmail.com>
2023-09-14 11:10:53 -0400

    add stochastic depth

--- focalnet_keras_core/layers.py
+++ focalnet_keras_core/layers.py
@@ -115,19 +115,15 @@ class LayerScale(keras.layers.Layer):
         return config
     
 class StochasticDepth(keras.layers.Layer):
-    """
-    https://keras.io/examples/vision/cct/
-    """
     def __init__(self, drop_prop, **kwargs):
-        super(StochasticDepth, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self.drop_prob = drop_prop
 
     def call(self, x, training=None):
         if training:
             keep_prob = 1 - self.drop_prob
-            shape = (tf.shape(x)[0],) + (1,) * (tf.shape(x).shape[0] - 1)
-            random_tensor = keep_prob + tf.random.uniform(shape, 0, 1)
-            random_tensor = tf.floor(random_tensor)
+            shape = (ops.shape(x)[0],) + (1,) * (len(x.shape) - 1)
+            random_tensor = keep_prob + keras.random.random.uniform(shape, 0, 1)
+            random_tensor = ops.floor(random_tensor)
             return (x / keep_prob) * random_tensor
         return x
-
