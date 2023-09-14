ac8ca40645855e898b537e05f23f85effa730367
Muhammad Anas Raza <memanasraza@gmail.com>
2023-09-14 11:10:12 -0400

    add initial stochastic depth

--- focalnet_keras_core/layers.py
+++ focalnet_keras_core/layers.py
@@ -113,4 +113,21 @@ class LayerScale(keras.layers.Layer):
             }
         )
         return config
+    
+class StochasticDepth(keras.layers.Layer):
+    """
+    https://keras.io/examples/vision/cct/
+    """
+    def __init__(self, drop_prop, **kwargs):
+        super(StochasticDepth, self).__init__(**kwargs)
+        self.drop_prob = drop_prop
+
+    def call(self, x, training=None):
+        if training:
+            keep_prob = 1 - self.drop_prob
+            shape = (tf.shape(x)[0],) + (1,) * (tf.shape(x).shape[0] - 1)
+            random_tensor = keep_prob + tf.random.uniform(shape, 0, 1)
+            random_tensor = tf.floor(random_tensor)
+            return (x / keep_prob) * random_tensor
+        return x
 
